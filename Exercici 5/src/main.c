9faaa0dd0342cf0dc9d09db79a732a7ad4b1b1e3
dan29iel <95416833+dan29iel@users.noreply.github.com>
2022-01-26 12:27:40 +0100

    Add files via upload

---
+++ Exercici 5/src/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: 26 ene. 2022
+ *      Author: daniel
+ */
+
+#include <stdio.h>
+#include "functions.h"
+
+void main(){
+	int num1, num2;
+	num1 = value();
+	num2 = value();
+	change(num1, num2);
+}
