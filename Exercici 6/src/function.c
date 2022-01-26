50090610384114acbd144567af1823a47c2641d8
dan29iel <95416833+dan29iel@users.noreply.github.com>
2022-01-26 12:27:54 +0100

    Add files via upload

---
+++ Exercici 6/src/function.c
@@ -0,0 +1,8 @@
+/*
+ * function.c
+ *
+ *  Created on: 26 ene. 2022
+ *      Author: daniel
+ */
+
+
---
+++ Exercici 6/src/function.h
@@ -0,0 +1,13 @@
+/*
+ * function.h
+ *
+ *  Created on: 26 ene. 2022
+ *      Author: daniel
+ */
+
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
+
+
+
+#endif /* FUNCTION_H_ */
