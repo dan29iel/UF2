8f2c959555928225dc8f9b56a723a0cd18bf2320
dan29iel <95416833+dan29iel@users.noreply.github.com>
2022-02-04 10:07:52 +0100

    Add files via upload

---
+++ Activitat 12/src/function.c
@@ -0,0 +1,8 @@
+/*
+ * function.c
+ *
+ *  Created on: 3 feb. 2022
+ *      Author: daniel
+ */
+
+
---
+++ Activitat 12/src/function.h
@@ -0,0 +1,13 @@
+/*
+ * function.h
+ *
+ *  Created on: 3 feb. 2022
+ *      Author: daniel
+ */
+
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
+
+
+
+#endif /* FUNCTION_H_ */
